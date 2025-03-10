a4afd94650866ca6185933fb0539f066e0387979
SabbirRahman71 <mdsabbirrahmanbd71@gmail.com>
2025-03-10 23:11:28 +0600

    initially completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/ui/Main";
+import Content from "./components/ui/Content";
+import Profile from "./profile/Profile";
+import Stats from "./components/stats/Stats";
+import Team from "./components/Team/Team";
+import Event from "./components/event/Event";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -16,6 +22,17 @@ function App() {
         toggleSideBar={toggleSideBar}
       />
       <Sidebar isSideBarOpen={isSideBarOpen} />
+
+      <Main>
+        <Content>
+          <Stats darkMode={darkMode} />
+          <div className="flex flex-col gap-3 lg:flex-row">
+            <Team />
+            <Event />
+          </div>
+        </Content>
+        <Profile />
+      </Main>
     </div>
   );
 }
